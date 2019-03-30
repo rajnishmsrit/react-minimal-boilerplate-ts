ab0bb80c64b9a63f1a018f2d41b5852b62630597
Rajnish Kumar <rajnishmsrit@gmail.com>
2019-03-31 01:51:00 +0530

    Updated component to tsx type

---
+++ src/Components/Test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Test extends React.Component {
+
+  public render() {
+    return (
+      <div>
+        ReactJS minimal boilerplate with Typescript 
+      </div>
+    );
+  }
+}
+
+export default Test;
